3659935b23294e2f5c9f8d6c18ec544669fbc6dc
adeline <adeline.lacabanne@gmail.com>
2023-10-22 14:04:47 +0200

    ok in postman

--- api_jam/src/Controller/RegistrationController.php
+++ api_jam/src/Controller/RegistrationController.php
@@ -2,43 +2,56 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\RegistrationFormType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
-class RegistrationController extends AbstractController
-{
-    #[Route('/register', name: 'register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+#[Route('/api')]
+class RegistrationController extends AbstractController {
+
+    private $userRepository;
+    private $serializer;
+    private $validator;
+    private $entityManager;
+
+    public function __construct(UserRepository $userRepository, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager)
     {
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+        $this->userRepository = $userRepository;
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+        $this->entityManager = $entityManager;
+    }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+    #[Route('/register', name: 'register', methods: ["POST"])]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher): JsonResponse {
+        
+        $data = $request->getContent();
+        $user = $this->serializer->deserialize($data, User::class, 'json');
 
-            $entityManager->persist($user);
-            $entityManager->flush();
-            // do anything else you need here, like send an email
+        $errors = $this->validator->validate($user);
 
-            return $this->redirectToRoute('login');
+        if (count($errors) > 0) {
+            $errorMessages = [];
+
+            foreach ($errors as $error) {
+                $errorMessages[] = $error->getMessage();
+            }
+
+            return new JsonResponse(['errors' => $errorMessages], 400);
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form,
-        ]);
+        $user->setPassword($userPasswordHasher->hashPassword($user, $user->getPassword()));
+
+        $this->entityManager->persist($user); // Utilisez l'EntityManager pour persister l'entité
+        $this->entityManager->flush(); // Utilisez l'EntityManager pour enregistrer les changements
+
+        return new JsonResponse(['message' => 'User created'], 201);
     }
 }
