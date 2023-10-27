8a84cb3c9ea695c17bb2351663792912b0089dcb
adeline <adeline.lacabanne@gmail.com>
2023-10-27 20:40:39 +0200

    no ok

--- api_jam/src/Controller/CheckoutController.php
+++ api_jam/src/Controller/CheckoutController.php
@@ -10,64 +10,82 @@ use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\JsonResponse; // Make sure to import JsonResponse
 
+#[Route('/api')]
 class CheckoutController extends AbstractController
 {
+
+    private $session;
+    private $productRepo;
+    private $orderRepo;
+    private $manager;
+
+    public function __construct(SessionInterface $session, ProductRepository $productRepo, OrderRepository $orderRepo, EntityManagerInterface $manager)
+    {
+        $this->session = $session;
+        $this->productRepo = $productRepo;
+        $this->orderRepo = $orderRepo;
+        $this->manager = $manager;
+    }
+
     #[IsGranted("ROLE_USER")]
-    #[Route('/checkout_success/{token}', name: 'checkout_success')]
-    public function checkout(EntityManagerInterface $manager, SessionInterface $session, OrderRepository $orderRepo, string $token)
+    #[Route('checkout_success/{token}', name: 'checkout_success',  methods: ['GET'])]
+    public function checkoutSuccess(string $token): JsonResponse
     {
         if ($this->isCsrfTokenValid('stripe_token', $token)) {
-            $order = $orderRepo->find($session->get("order_waiting"));
-            $order->setStatus("PAYMENT_OK");
-            $manager->flush();
-            return $this->render('checkout/success.html.twig', [
-                "order" => $order
-            ]);
-        } else {
-            return $this->render('checkout/error.html.twig', []);
+            $orderId = $this->session->get("order_waiting");
+            $order = $this->orderRepo->find($orderId);
+
+            if ($order) {
+                $order->setStatus("PAYMENT_OK");
+                $this->manager->flush();
+
+                return $this->json(['message' => 'Payment successful', 'order' => $order]);
+            }
         }
-    }
 
-    #[IsGranted("ROLE_USER")]
-    #[Route('/checkout_error', name: 'checkout_error')]
-    public function error()
-    {
-        return $this->render('checkout/error.html.twig', []);
+        return $this->json(['error' => 'Invalid CSRF token'], 400);
 
     }
 
     #[IsGranted("ROLE_USER")]
-    #[Route('/checkout', name: 'checkout')]
-    public function index(EntityManagerInterface $manager, ProductRepository $productRepo, SessionInterface $session)
+    #[Route('/checkout_error', name: 'checkout_error', methods: ['GET'])]
+    public function error(): JsonResponse
     {
-        $tokenProvider = $this->container->get('security.csrf.token_manager');
-        $token = $tokenProvider->getToken('stripe_token')->getValue();
+        return $this->json(['error' => 'Payment error'], 400);
+
+    }
+
+    #[IsGranted("ROLE_USER")]
+    #[Route('/checkout', name: 'checkout', methods: ['POST'])]
+    public function index(): JsonResponse
+    {
         $stripe_items = [];
-        $cart = $session->get("cart", []);
+        $cart = $this->session->get("cart", []);
+
         if (empty($cart)) {
-            return $this->redirectToRoute("home");
+            return $this->json(['error' => 'Cart is empty'], 400);
         }
+
         $order = new Order;
         $order->setDatetime(new DateTime);
         $order->setStatus("PAYMENT_WAITING");
         $total = 0;
 
         foreach ($cart as $key => $quantity) {
-            $product = $productRepo->find($key);
-            $line = new LineOrder;
-            $line->setProduct($product);
-            $line->setQuantity($quantity);
-            $line->setSubtotal($quantity * $product->getPrice());
-            $total += $quantity * $product->getPrice();
-            $order->addLineOrder($line);
-            //equivalent
-            // $line->setOrderAssociated($order);
-            $stripe_items[] =
-                [
+            $product = $this->productRepo->find($key);
+
+            if ($product) {
+                $line = new LineOrder;
+                $line->setProduct($product);
+                $line->setQuantity($quantity);
+                $line->setSubtotal($quantity * $product->getPrice());
+                $total += $quantity * $product->getPrice();
+                $order->addLineOrder($line);
+                $stripe_items[] = [
                     'price_data' => [
                         'currency' => 'eur',
                         'product_data' => [
@@ -77,20 +95,20 @@ class CheckoutController extends AbstractController
                     ],
                     'quantity' => $quantity,
                 ];
+            }
         }
+
         $order->setTotal($total);
-        $manager->persist($order);
-        $manager->flush();
-        $session->set("order_waiting", $order->getId());
+        $this->manager->persist($order);
+        $this->manager->flush();
+        $this->session->set("order_waiting", $order->getId());
 
         \Stripe\Stripe::setApiKey($_ENV["STRIPE_API_KEY"]);
         $session = \Stripe\Checkout\Session::create([
             'line_items' => $stripe_items,
-            'mode' => 'payment',
-            'success_url' => 'http://localhost:8000/checkout_success/' . $token,
-            'cancel_url' => 'http://localhost:8000/checkout_error'
+            'mode' => 'payment'
         ]);
 
-        return $this->redirect($session->url, 303);
+        return $this->json(['message' => 'Checkout initiated', 'checkout_url' => $session->url]);
     }
 }
